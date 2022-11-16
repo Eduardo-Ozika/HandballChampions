0beba96c36d96b639f9580c422d8e53fca864946
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-11-16 18:01:11 -0300

    criando o repositorio de endereço

---
+++ src/main/java/br/com/crp/handeballCamp/model/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package br.com.crp.handeballCamp.model.repository;
+
+
+import br.com.crp.handeballCamp.model.entity.Address;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address,Long> {}
