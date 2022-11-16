2569dd02503ca69e259114c5b6a8857e7f8587e3
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-11-16 18:03:03 -0300

    pequenas alteraçoes

--- src/main/java/br/com/crp/handeballCamp/main.java
+++ src/main/java/br/com/crp/handeballCamp/main.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HandeballCampApplication {
-
+public class main {
+	static boolean system = true;
 	public static void main(String[] args) {
-		SpringApplication.run(HandeballCampApplication.class, args);
+		SpringApplication.run(main.class, args);
+
+		while (system){
+
+		}
 	}
 
 }
